7eaa0d7901d3f52aa37c302976b0471fbf36a2e5
nownabe <nownabe@gmail.com>
2016-11-13 23:02:52 +0900

    :shower: Use stack pointer

--- lib/nyarv/virtual_machine.rb
+++ lib/nyarv/virtual_machine.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 require "nyarv/instruction_sequence"
+require "nyarv/instructions_loader"
 
 module Nyarv
   class VirtualMachine
     attr_reader :iseq, :scope, :stack
+    attr_reader :pc, :sp, :cfp, :lfp, :dfp
 
     def initialize(source)
       @iseq = InstructionSequence.from(source)
       @stack = []
       @scope = Kernel
-      @pc = 0
+      @pc = @sp = 0
+      @cfp = @lfp = @dfp = nil
+      @instructions = InstructionsLoader.new(self)
     end
 
     def run
@@ -22,12 +26,22 @@ module Nyarv
       end
     end
 
+    def pop
+      @sp -= 1
+      stack[@sp]
+    end
+
+    def push(val)
+      stack[@sp] = val
+      @sp += 1
+    end
+
     private
 
     def execute(instruction)
       $stderr.puts "Execute\t#{instruction}"
       send(instruction.opecode, *instruction.operands)
-      $stderr.puts "\t(Stack: #{stack.map(&:inspect).join(', ')})"
+      $stderr.puts "\t(Stack: #{stack[0...@sp].map(&:inspect).join(', ')})"
     rescue NoMethodError => e
       $stderr.puts "\t(Not implemented instruction '#{e.name}')"
     end
@@ -35,19 +49,19 @@ module Nyarv
     # instructions
 
     def putself
-      stack.push scope
+      push scope
     end
 
     def putstring(string)
-      stack.push string
+      push string
     end
 
     def opt_send_without_block(*args)
       method_name = args[0][:mid]
       argc = args[0][:orig_argc]
-      args = Array.new(argc) { stack.pop }
-      receiver = stack.pop
-      stack.push receiver.send(method_name, *args)
+      args = Array.new(argc) { pop }
+      receiver = pop
+      push receiver.send(method_name, *args)
     end
   end
 end
